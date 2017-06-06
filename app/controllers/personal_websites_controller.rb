9eaf5fb4df9ab2195be5ad2c9016d9242211d082
shiva <bhattaraishiva17@gmail.com>
2017-06-06 06:33:49 +0300

    added other static pages in controller and routes

--- app/controllers/personal_websites_controller.rb
+++ app/controllers/personal_websites_controller.rb
@@ -1,4 +1,16 @@
 class PersonalWebsitesController < ApplicationController
   def home
   end
+
+  def skill
+  end
+
+  def education
+  end
+
+  def experience
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'personal_websites/skill'
+  get 'personal_websites/education'
+  get 'personal_websites/experience'
+  get 'personal_websites/about'
+  
   root 'personal_websites#home'
 end
