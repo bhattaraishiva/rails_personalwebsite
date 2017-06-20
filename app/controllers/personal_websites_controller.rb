c4fc41b55f52a399b02935f0b2e9b788c43d1cfd
shiva <bhattaraishiva17@gmail.com>
2017-06-21 01:28:01 +0300

    work on about form

--- app/controllers/personal_websites_controller.rb
+++ app/controllers/personal_websites_controller.rb
@@ -12,5 +12,15 @@ class PersonalWebsitesController < ApplicationController
   end
 
   def about
+    @about = About.new
+    if request.post?
+      @about = About.new(about_params)
+      @about.save
+      redirect_to root_path
+      flash[:notice] = "Thank you for your message"
+    end
+  end
+  def about_params
+    params.require(:about).permit(:name, :email, :phone, :message)
   end
 end
