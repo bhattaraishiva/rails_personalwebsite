77e0ffd1d70962050a90d9926094474a8bfce969
shiva <bhattaraishiva17@gmail.com>
2017-06-28 12:34:15 +0300

    work on work and experience

--- app/controllers/personal_websites_controller.rb
+++ app/controllers/personal_websites_controller.rb
@@ -2,9 +2,6 @@ class PersonalWebsitesController < ApplicationController
   def home
   end
 
-  def skill
-  end
-
   def education
   end
 
@@ -20,6 +17,10 @@ class PersonalWebsitesController < ApplicationController
       flash[:notice] = "Thank you for your message"
     end
   end
+  def download_resume
+    send_file "#{Rails.root}/app/assets/docs/resume.pdf", type: "application/pdf"
+  end
+
   def about_params
     params.require(:about).permit(:name, :email, :phone, :message)
   end
